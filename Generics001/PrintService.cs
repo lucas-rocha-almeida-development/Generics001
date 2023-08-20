74cd30f1bd62277a8299af3c2820455d0d9694b7
Lucas Almeida <lucalmr12@gmail.com>
2023-08-20 19:54:08 -0300

    Adicionar arquivos de projeto.

---
+++ Generics001/PrintService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics001
+{
+    internal class PrintService<T>
+    {//variavel interna que podera armazenar 10 numeros inteiros
+        private  T[] _values = new T[10];
+
+        private int _count = 0;//variavel responsavel para fazer a contagem dos numeros 
+
+        //criar função agora metodo de adicionar
+
+        public void AddValue (T value)
+        {
+
+            if(_count == 10 )
+            {
+                throw new InvalidOperationException("Print servece já esta cheio !!!");
+
+            }
+            //meu vetor chamado: _value na posição _count ira receber o valor 
+            //meu _vetor = tem 10 posiçoes de 0 ate 9
+            //
+            _values[_count] = value;
+            _count++;//atualizar a variavel count e necessario
+        }
+
+        //metodo para retornar o primeiro elemento do meu projeto (PrintServece)
+
+        public T First()
+        {   //programação defensiva ( exceção)
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("Print servece esta vazio !!!");
+
+            }
+
+            return _values[0];
+        }
+        public void print ()
+        {
+            Console.Write($" [ ");
+
+            for (int i = 0; i < _count -1;i++) {
+                
+                //paga cada elemento imprima
+                Console.Write(_values[i] + ", " );
+            
+            }
+            //imprimir o ultimo valor 
+            if(_count > 0 ) {
+                Console.Write(_values[_count -1]);
+
+            }
+
+            Console.WriteLine("]");
+
+
+        }
+    }
+}
---
+++ Generics001/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Generics001;
+
+namespace Generics001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           PrintService<int> printService = new PrintService<int>();
+
+            Console.Write("Quantos valores voce ira digitar : ");
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+
+               int x = int.Parse(Console.ReadLine());
+
+                printService.AddValue(x);
+                
+            }
+
+    
+            //imprimir todos os elementos
+            printService.print();
+            Console.WriteLine("Primeiro valor: " + printService.First());
+        }
+    }
+}
